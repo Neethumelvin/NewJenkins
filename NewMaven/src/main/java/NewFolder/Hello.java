8ab566fc7f2d2b64655e790d959168c41d6d0567
Lenovo <Lenovo@DESKTOP-3JDDFLH>
2023-07-31 11:56:19 +0530

    Hello

---
+++ NewMaven/src/main/java/NewFolder/Hello.java
@@ -0,0 +1,15 @@
+package NewFolder;
+
+public class Hello {
+	public void display()
+	{
+		System.out.println("Hello Neethu");
+	}
+
+	public static void main(String[] args) {
+		
+		Hello he=new Hello();
+		he.display();
+	}
+
+}
